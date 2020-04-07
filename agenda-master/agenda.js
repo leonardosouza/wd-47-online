e4d0ab92a8bcf5245f84c2e9e08a96d936593445
Leonardo Souza <leonardo.souza@c6bank.com>
2020-04-06 23:06:57 -0300

    aula 04

--- agenda-master/agenda.js
+++ agenda-master/agenda.js
@@ -1,1 +1,52 @@
-console.log("=== AGENDA ===");
+(function() {
+    console.log("Starting APP...");
+
+    // UI (User Interface)
+    var ui = {
+        fields: document.querySelectorAll("input"),
+        buttonAdd: document.querySelector(".btn-add"),
+        tableContacts: document.querySelector(".table-contacts")
+    };
+
+    // Actions
+    var validateFields = function(e) {
+        e.preventDefault();
+
+        var errors = 0;
+
+        var contact = {};
+
+        ui.fields.forEach(function(field) {
+            if(field.value.length == 0) {
+                field.classList.add("error");
+                errors++;
+            } else {
+                field.classList.remove("error");
+                contact[field.id] = field.value;
+            }
+        });
+
+        if(errors > 0) {
+            document.querySelector(".error").focus();
+        } else {
+            addContact(contact);
+        }
+    };
+
+    var addContact = function() {
+        debugger;
+    };
+
+    var getContacts = function() {};
+
+    var removeContact = function() {};
+
+    var init = function() {
+        // Mapping Event
+        ui.buttonAdd.onclick = validateFields;
+
+        // debugger;
+    }();
+
+    // console.log(ui);
+})();
